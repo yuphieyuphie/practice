c92446b99b54c092878e9eed74c212ebd78eae30
yuphieyuphie <33336711+yuphieyuphie@users.noreply.github.com>
2018-05-05 17:36:19 -0400

    Update Array.py

--- Easy Collection/Array.py
+++ Easy Collection/Array.py
@@ -64,3 +64,27 @@ class Solution(object):
         :rtype: int
         """
         return (sum(set(nums)))*2-sum(nums)
+
+###Intersection of Two Arrays II###    
+    class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1=sorted(nums1)
+        nums2=sorted(nums2)
+        a=0
+        b=0
+        res=[]
+        while a<len(nums1) and b<len(nums2):
+            if nums1[a]==nums2[b]:
+                res.append(nums1[a])
+                a+=1
+                b+=1
+            elif nums1[a]<nums2[b]:
+                a+=1
+            else:
+                b+=1
+        return res
