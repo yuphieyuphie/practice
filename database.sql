330b16d2d248ecf508cb5cc31e0310bd4edc3640
yuphieyuphie <33336711+yuphieyuphie@users.noreply.github.com>
2018-06-13 22:46:31 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,101 @@
+/***Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| PersonId    | int     |
+| FirstName   | varchar |
+| LastName    | varchar |
++-------------+---------+
+PersonId is the primary key column for this table.
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| AddressId   | int     |
+| PersonId    | int     |
+| City        | varchar |
+| State       | varchar |
++-------------+---------+
+AddressId is the primary key column for this table.
+ 
+
+Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+
+FirstName, LastName, City, State***/
+
+SELECT p.FirstName, p.LastName, a.City, a.State
+FROM Person AS p 
+LEFT JOIN Address AS a
+ON p.PersonId=a.PersonId
+
+/***Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+ ***/
+
+Select MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary<(SELECT MAX(Salary)
+                FROM Employee)
+
+/***Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Note: All emails are in lowercase.***/
+
+SELECT Email 
+FROM Person
+Group BY Email
+HAVING COUNT(Email)>1
+
+/***The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+***/
+
+SELECT a.Name AS Employee
+FROM Employee AS a 
+JOIN Employee AS b
+ON a.ManagerId=b.Id
+WHERE a.Salary>b.Salary
 
